b83dfb899089169d2f010e0a9966ab3893a64ce7
valentina Ramirez <valeramirez1557@gmail.com>
2019-08-13 14:34:59 -0500

    universidades

---
+++ WebAppAWIES/Datos/clUniversidades.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppAWIES.Datos
+{
+    public class clUniversidades
+    {
+        public int IdUnversiades{ get; set; }
+        public string Codigo { get; set; }
+        public string EstadoSeccional { get; set; }
+        public string Sector { get; set; }
+        public string Nit { get; set; }
+        public string Correo { get; set; }
+        public string EstadoSolicitud { get; set; }
+        public string Contraseña { get; set; }
+        public string NombreInstitucion { get; set; }
+        public string Acreditada { get; set; }
+        public string PrincipalSeccional { get; set; }
+        public int IdCaracterAcademico { get; set; }
+        public int IdMunicipio { get; set; }
+        public int IdPrograma { get; set; }
+    }
+}

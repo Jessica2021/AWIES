522a7b9487db3ff210f9aac2c9c0e397a2c43882
Jessica2021 <44820895+Jessica2021@users.noreply.github.com>
2019-08-15 13:56:09 -0500

    vistas2

--- WebAppAWIES/Registrar.aspx.cs
+++ WebAppAWIES/Registrar.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace WebAppAWIES
 {
-    public partial class EnvioSolicitudd : System.Web.UI.Page
+    public partial class Registrar1 : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
--- WebAppAWIES/Registrar.aspx.designer.cs
+++ WebAppAWIES/Registrar.aspx.designer.cs
@@ -10,6 +10,15 @@
 namespace WebAppAWIES {
     
     
-    public partial class Registro {
+    public partial class Registrar1 {
+        
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
     }
 }
